4df9de5e72f39a471a160f3a9bdd35227a9c115d
Laura THAY <laura.thay@ipag.fr>
2021-04-15 21:34:34 +0200

    exercise 1 half+++

---
+++ 01_Day_Introduction/01_day_starter/datatypebis.js
@@ -0,0 +1,1 @@
+let laura = 9;
--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -7,3 +7,15 @@ console.log(challenge.length);
 console.log(challenge.toUpperCase());
 console.log(challenge.toLowerCase());
 console.log(challenge.substr(1));
+console.log(challenge.substring(3,22));
+console.log(challenge.includes('Script'));
+console.log(challenge.split(''));
+console.log(challenge.split(', '))
+console.log(challenge.replace('JavaScript', 'Python'));
+console.log(challenge.charAt(15));
+console.log(challenge.charCodeAt('J'));
+console.log(challenge.indexOf('a'));
+console.log(challenge.lastIndexOf('a'));
+
+let sentence = 'You cannot end a sentence with because because because is a conjunction';
+console.log(sentence.indexOf('because'));
