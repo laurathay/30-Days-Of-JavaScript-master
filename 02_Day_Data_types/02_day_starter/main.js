e28b4db190c1a44369f8973951b79b00e0afb460
Laura THAY <laura.thay@ipag.fr>
2021-04-15 20:59:49 +0200

    partie exo

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -1,1 +1,9 @@
 // this is your main.js script
+
+let challenge = '30 days of JavaScript';
+
+console.log(challenge);
+console.log(challenge.length);
+console.log(challenge.toUpperCase());
+console.log(challenge.toLowerCase());
+console.log(challenge.substr(1));
